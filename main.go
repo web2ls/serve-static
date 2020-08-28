b5de7bbda2179cb7aff6a4983977ab6af6a9b371
web2ls <web2ls@yandex.ru>
2020-08-28 15:46:35 +0300

    add basic static files server

--- main.go
+++ main.go
@@ -2,8 +2,21 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
+	"os"
 )
 
 func main() {
-	fmt.Println("Static files server")
+	const PORT = ":8080"
+	dir, err := os.Getwd()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fs := http.FileServer(http.Dir(dir))
+	http.Handle("/", fs)
+
+	fmt.Println("Server has been started on port " + PORT)
+	http.ListenAndServe(PORT, nil)
 }
